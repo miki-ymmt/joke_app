9acd563bc0a3d16c45121839b35589aa36aa924b
山本美希 <yamamotomiki@yamamotikinoAir.AirPort>
2024-05-31 13:40:32 +0900

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const { default: daisyui } = require("daisyui");
+
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "nord"],
+  },
+}
