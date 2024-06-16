d50a8aa7834fb11ddf213b38ee952113312324cc
山本美希 <yamamotomiki@yamamotikinoAir.AirPort>
2024-06-16 18:25:02 +0900

    アニメーションの追加とログイン済みの場合にボタンを押したらnew_jokeに遷移するように変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,28 @@ module.exports = {
     extend: {
       fontFamily: {
         'noto': ['Noto Sans JP', 'sans-serif'],
-        'kaisei': ['Kaisei Tokumin', 'san-serif']
+        'kaisei': ['Kaisei Tokumin', 'sans-serif']
       },
       fontSize: {
         'xxs': '0.625rem',
       },
+      animation: {
+        "tilt-in-top-1": "tilt-in-top-1 1.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both"
+      },
+      keyframes: {
+        "tilt-in-top-1": {
+            "0%": {
+                transform: "rotateY(30deg) translateY(-300px) skewY(-30deg)",
+                opacity: "0"
+            },
+            "100%": {
+                transform: "rotateY(0deg) translateY(0) skewY(0deg)",
+                opacity: "1"
+            }
+        }
+      }
     },
- },
+  },
   plugins: [require("daisyui")],
   daisyui: {
     themes: ["light", "dark", "nord"],
